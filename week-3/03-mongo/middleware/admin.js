e4600a7c45abd24b0fe2c8237bf50a814f9a2eab
Rithik53 <rithikkumar53@gmail.com>
2023-12-30 02:40:33 +0530

    mongo db

--- week-3/03-mongo/middleware/admin.js
+++ week-3/03-mongo/middleware/admin.js
@@ -5,27 +5,43 @@ const bcrypt = require("bcrypt");
 async function adminMiddleware(req, res, next) {
     // Implemented admin auth logic
     // Checking the headers and validate the admin from the admin DB.
-    try {
-        const { username, password } = req.headers;
 
-        //checking if username exists
-        const admin = await Admin.findOne({ username });
+    const username = req.headers.username;
+    const password = req.headers.password;
+    Admin.findOne({
+      username: username,
+      password: password
+    })
+    .then(function(value){
+      if(value){
+        next();
+      } else{
+         res.status(403).json({
+          msg: "user doesn't exist"
+         })
+      }
+    })
+    // try {
+    //     const { username, password } = req.headers;
 
-        if (!admin) return res.status(401).json({ message: 'Unauthorized' });
-        else {
-          //checking if password is correct
-          const passwordValidates = bcrypt.compare(
-            password,
-            admin.password
-          );
+    //     //checking if username exists
+    //     const admin = await Admin.findOne({ username });
 
-          if (passwordValidates) next();
-          else return res.status(401).json({ message: 'Unauthorized' });
-        }
+    //     if (!admin) return res.status(401).json({ message: 'Unauthorized' });
+    //     else {
+    //       //checking if password is correct
+    //       const passwordValidates = bcrypt.compare(
+    //         password,
+    //         admin.password
+    //       );
 
-    }catch (e) {
-        res.status(401).send();
-    }
+    //       if (passwordValidates) next();
+    //       else return res.status(401).json({ message: 'Unauthorized' });
+    //     }
+
+    // }catch (e) {
+    //     res.status(401).send();
+    // }
 
 }
 
--- week-3/03-mongo/middleware/user.js
+++ week-3/03-mongo/middleware/user.js
@@ -4,23 +4,42 @@ const bcrypt = require('bcrypt');
 async function userMiddleware(req, res, next) {
   // Implemented user auth logic
   // Checking the headers and validating the user from the user DB.
-  try {
-    const { username, password } = req.headers;
-
-    //checking if username exists
-    const user = await User.findOne({ username });
-
-    if (!user) return res.status(401).json({ message: 'Unauthorized' });
-    else {
-      //checking if password is correct
-      const passwordValidates = bcrypt.compare(password, user.password);
-
-      if (passwordValidates) next();
-      else return res.status(401).json({ message: 'Unauthorized' });
+  const username = req.headers.username;
+  const password = req.headers.password;
+  User.findOne({
+    username: username,
+    password: password
+  })
+  .then(function(value){
+    if(value){
+      next();
+    } else{
+       res.status(403).json({
+        msg: "user doesn't exist"
+       })
     }
-  } catch (e) {
-    res.status(401).send();
-  }
+  })
+
+
+
+
+  // try {
+  //   const { username, password } = req.headers;
+
+  //   //checking if username exists
+  //   const user = await User.findOne({ username });
+
+  //   if (!user) return res.status(401).json({ message: 'Unauthorized' });
+  //   else {
+  //     //checking if password is correct
+  //     const passwordValidates = bcrypt.compare(password, user.password);
+
+  //     if (passwordValidates) next();
+  //     else return res.status(401).json({ message: 'Unauthorized' });
+  //   }
+  // } catch (e) {
+  //   res.status(401).send();
+  // }
 }
 
 module.exports = userMiddleware;
